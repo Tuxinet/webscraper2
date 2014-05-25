85ce4d7622b63aa2173c784363dee3fc2b77a2c8
Tuxinet <tuxinet32@gmail.com>
2014-05-25 20:35:47 +0200

    Bugs

--- StandaloneScraper.py
+++
@@ -1,8 +0,0 @@
-import scraper
-
-if __name__ == '__main__':
-    url = raw_input("Input url: ")
-    limit = input("Input number of urls to visit: ")
-    sleeptime = input("Input sleeptime: ")
-    spider = scraper.Scraper(url, limit, sleeptime)
-    spider.crawl()
